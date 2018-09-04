cd581dabcc94234a964df39a82567edba1664821
rsotani <sosososotaniryota@gmail.com>
2018-09-05 01:05:07 +0900

    Solved ABC054 C and make DFS lib

--- BeginnerContest/050-059/054/C_OneStrokePath.cpp
+++ BeginnerContest/050-059/054/C_OneStrokePath.cpp
@@ -1,21 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int dps(unorderd_map mp, set alp){
+int N, M;
+map<int, set<int> > mp;
+int ans = 0;
+set<int> alp;
 
+void dps(int nxt){
+  alp.insert(nxt);
+  if (alp.size() == N){
+    ans++;
+  }
+  else{
+    for (int x: mp[nxt]){
+      if (alp.find(x) == alp.end()){
+        dps(x);
+      }
+    }
+  }
+  alp.erase(nxt);
 }
 
 int main(){
-  int N, M;
   cin >> N >> M;
-  unorderd_map<int, set<int> > mp;
   for (int i=0; i<M; i++){
     int a, b;
     cin >> a >> b;
     mp[a].insert(b);
     mp[b].insert(a);
   }
-  set<int> alp;
-  alp.insert(1);
-
+  dps(1);
+  cout << ans << endl;
 }
