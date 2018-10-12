3e34e94a51bbde4c74243a94bf73d699ef5d6b1d
rsotani <sosososotaniryota@gmail.com>
2018-10-13 01:48:33 +0900

    Solved ABC005 C

---
+++ BeginnerContest/001-009/005/C.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int T, N;
+  cin >> T >> N;
+  vector<int> A(N);
+  for (int i=0; i<N; i++) cin >> A[i];
+  int M;
+  cin >> M;
+  vector<int> B(M);
+  for (int i=0; i<M; i++) cin >> B[i];
+  int a=0, b=0;
+  while(a<N){
+    if (A[a]>B[b]){
+      cout << "no" << endl;
+      return 0;
+    }
+    if (A[a]+T<B[b]){
+      while(A[a]+T<B[b]&&a<N){
+        a++;
+      }
+    }
+    if (a<N&&A[a]+T>=B[b]){
+      a++; b++;
+    }
+  }
+  if (a==N&&b<M) cout << "no" << endl;
+  else cout << "yes" << endl;
+}
