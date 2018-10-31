9998db57158e00ac759caffa22d4491652a1ec0b
rsotani <sosososotaniryota@gmail.com>
2018-10-31 10:15:22 +0900

    Solved ARC048 B

--- RegularContest/040-049/048/B.cpp
+++ RegularContest/040-049/048/B.cpp
@@ -4,11 +4,25 @@ using namespace std;
 int main(){
   int N;
   cin >> N;
-  map<int, vector<int>(3,0) > mp;
+  vector<vector<int> > RH(N, vector<int>(2));
+  map<int, vector<int> > mp;
+  vector<int> R(N);
   for (int i=0; i<N; i++){
     int r, h;
     cin >> r >> h;
-    mp[]
+    if (mp[r].size()!=3) mp[r] = {0,0,0};
+    RH[i][0] = r; RH[i][1] = h;
+    R[i] = r;
+    mp[r][h-1]++;
   }
-
+  sort(R.begin(), R.end());
+  for (int i=0; i<N; i++){
+    int w=0, d=0, l=0;
+    w += lower_bound(R.begin(), R.end(), RH[i][0]) - R.begin();
+    l += N - (lower_bound(R.begin(), R.end(), RH[i][0]+1) - R.begin());
+    w += mp[RH[i][0]][(RH[i][1])%3];
+    d += mp[RH[i][0]][(RH[i][1]-1)%3]-1;
+    l += mp[RH[i][0]][(RH[i][1]+1)%3];
+    cout << w << ' ' << l << ' ' << d << endl;
+  }
 }
