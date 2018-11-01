2ca6c27a81ddcef86f4e99ce60c6ba1b4a9f7f59
rsotani <sosososotaniryota@gmail.com>
2018-11-02 00:25:29 +0900

    Solved ARC103 E

--- RegularContest/100-109/103/E.cpp
+++ RegularContest/100-109/103/E.cpp
@@ -5,17 +5,26 @@ int main(){
   string s;
   cin >> s;
   int slen = s.size();
-  if (s[slen-1]=='1'){
-    cout << '-1' << endl;
+  if (s[slen-1]=='1'||s[0]=='0'){
+    cout << -1 << endl;
     return 0;
   }
   for (int i=0; i<slen-1; i++){
     if (s[i] != s[slen-2-i]){
-      cout << '-1' << endl;
+      cout << -1 << endl;
       return 0;
     }
   }
-  for (int i=0; i<(slen-1)/2; i++){
-    
+  vector<int> lis;
+  for (int i=0; i<slen-1; i++){
+    if (s[i]=='1'){
+      for (auto k: lis){
+        cout << i+1 << ' ' << k << endl;
+      }
+      lis.clear();
+      lis.push_back(i+1);
+    }
+    else lis.push_back(i+1);
   }
+  cout << slen-1 << ' ' << slen << endl;
 }
