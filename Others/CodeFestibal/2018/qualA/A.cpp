f6c92a41978bff5f4b147c658af837644336ff86
rsotani <sosososotaniryota@gmail.com>
2018-11-25 22:43:09 +0900

    modify filename

--- Others/CodeFestibal/2018/qualA/A.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-  int A,B,C,S;
-  cin >> A >> B >> C >> S;
-  if ((A+B+C<=S)&&(A+B+C+3>=S)) cout << "Yes" << endl;
-  else cout << "No" << endl;
-}
--- Others/CodeFestibal/2018/qualB/A.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-  int N;
-  cin >> N;
-  cout << 100-100/N << endl;
-}
