8a334fde251b740277b1bdbea4505f0767b8ba58
rsotani <sosososotaniryota@gmail.com>
2018-11-27 17:06:40 +0900

    Solved Petrozavodsk B C

--- Others/Petrozavodsk/001/B.cpp
+++ Others/Petrozavodsk/001/B.cpp
@@ -5,4 +5,27 @@ int main(){
   int N;
   cin >> N;
   vector<int> a(N), b(N);
+  for (int i=0; i<N; i++) cin >> a[i];
+  for (int i=0; i<N; i++) cin >> b[i];
+  long long OP = 0, asum = 0, bsum = 0;
+  for (int i=0; i<N; i++){
+    asum += a[i];
+    bsum += b[i];
+  }
+  OP = bsum-asum;
+  long long add1 = 0, add2=0, lstop=0;
+  for (int i=0; i<N; i++){
+    if (a[i] < b[i]){
+      add2 += (b[i]-a[i])/2;
+      if ((b[i]-a[i])%2==1) add2++;
+    } else if (a[i] > b[i]){
+      add1 += (a[i]-b[i])/2;
+    }
+  }
+  lstop=max(add1, add2);
+  if (lstop<=OP){
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
 }
---
+++ Others/Petrozavodsk/001/C.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int N;
+  cin >> N;
+  cout << 0 << endl;
+  string befres;
+  cin >> befres;
+  if (befres=="Vacant"){
+    return 0;
+  }
+  int befnum = 0;
+  int u=N-1, l=0;
+  while (true){
+    int check = (u+l)/2;
+    if (check==befnum){
+      check++;
+    }
+    string res;
+    cout << check << endl;
+    cin >> res;
+    if (res == "Vacant"){
+      return 0;
+    }
+    if (abs(check-befnum)%2==0){
+      if (res==befres){
+        l = check;
+      } else {
+        u = check;
+      }
+    } else {
+      if (res==befres){
+        u = check;
+      } else {
+        l = check;
+      }
+    }
+    befres = res;
+    befnum = check;
+  }
+}
