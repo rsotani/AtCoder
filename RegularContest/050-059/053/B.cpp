cf7eb893e73842c56997906e788138a228bacbfc
rsotani <sosososotaniryota@gmail.com>
2018-12-05 05:46:16 +0900

    Solved ARC053 B

--- RegularContest/050-059/053/B.cpp
+++ RegularContest/050-059/053/B.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main(){
-  
+  string S;
+  cin >> S;
+  vector<int> num(26);
+  for (int i=0; i<S.size(); i++){
+    num[S[i]-'a']++;
+  }
+  int oddnum = 0;
+  for (int i=0; i<26; i++){
+    if (num[i]%2==1){
+      oddnum++;
+    }
+  }
+  int ans;
+  if (oddnum<=1){
+    ans = S.size();
+  } else {
+    ans = S.size()/oddnum;
+    if (ans%2==0) ans--;
+  }
+  cout << ans << endl;
 }
